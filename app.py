d4e9b0360b1a803e186816ad46918276ddf67c14
vicky <vickyji@users.noreply.huggingface.co>
2023-09-20 13:01:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import numpy as np
 cnnmodel = keras.models.load_model("cnnmodel2")
 
 def to_predict(img):
-  img_array = tf.keras.preprocessing.image.img_to_array(img)
+  #img_array = tf.keras.preprocessing.image.img_to_array(img)
+  img_array = tf.keras.utils.img_to_array(img)
   img_array = tf.expand_dims(img_array, 0)
   predictions = cnnmodel.predict(img_array)
   conf= {}
@@ -20,8 +21,8 @@ import gradio as gr
 
 iface=gr.Interface(fn=to_predict,
              inputs=gr.Image(),
-             outputs=gr.Label(num_top_classes=3))#,
-             #examples=["images/image(9).jpg","images/image.jpg","images/p (1).jpg","images/gg (1).jpg"])
+             outputs=gr.Label(num_top_classes=3)),
+             examples=["images/image(9).jpg","images/image.jpg","images/p (1).jpg","images/gg (1).jpg"])
 
 iface.launch()
     
